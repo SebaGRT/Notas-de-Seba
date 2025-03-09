034c4fb5e4cb9505a0e2b424801ec67e725e2cbd
SebaGRT <coke2014@outlook.cl>
2025-03-09 15:31:22 -0300

    Quartz sync: Mar 9, 2025, 3:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Notas de Seba🎓",
+    pageTitle: "Notas de Seba 🎓",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
